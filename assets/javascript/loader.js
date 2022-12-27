35f435b3b1ef50f416859f10cc762268206bbc5e
matthew-hornby <120569152+matthew-hornby@users.noreply.github.com>
2022-12-27 22:16:03 +0000

    Initial backup commit

---
+++ assets/javascript/loader.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Will wait for everything on the page to load.
+    $(window).bind('load', function() {
+        $('.overlay, body').addClass('loaded');
+        setTimeout(function() {
+            $('.overlay').css({'display':'none'})
+        }, 2000)
+    });
+    
+    // Will remove overlay after 1min for users cannnot load properly.
+    setTimeout(function() {
+        $('.overlay, body').addClass('loaded');
+    }, 60000);
+})
